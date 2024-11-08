3d76d363e8435842f2839164ed393ca4b9d05ff8
Thien-Bao Bui <80117447+tbbui-732@users.noreply.github.com>
2024-11-08 15:02:17 -0500

    feat: initialize glfw version, window, glad pointer, and viewport dimensions

--- LearnOpenGL/main.cpp
+++ LearnOpenGL/main.cpp
@@ -2,8 +2,35 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+const int scrHeight = 800;
+const int scrWidth	= 600;
+
 int main(void)
 {
-	std::cout << "Test" << std::endl;
+	// initialize glfw version and profile
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	// create glfw window
+	GLFWwindow* window = glfwCreateWindow(scrHeight, scrWidth, "LearnOpenGL", NULL, NULL);
+	if (window == nullptr) {
+		std::cerr << "Failed to initialize GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	else {
+		glfwMakeContextCurrent(window);
+	}
+
+	// set up glad pointer
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		std::cerr << "Failed to initialize GLAD" << std::endl;
+	}
+
+	// set up viewport dimensions
+	glViewport(0, 0, scrHeight, scrWidth);
+
 	return 0;
 }
