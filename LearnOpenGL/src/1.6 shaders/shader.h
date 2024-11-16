c827fb11578160d1fa5fec179ec939d56eefcf6a
Thien-Bao Bui <80117447+tbbui-732@users.noreply.github.com>
2024-11-16 16:25:09 -0500

    Read shader code stream from file buffer and convert to string

--- LearnOpenGL/src/1.6 shaders/shader.h
+++ LearnOpenGL/src/1.6 shaders/shader.h
@@ -10,7 +10,38 @@ class Shader {
 public:
 	unsigned int ID;
 
-	Shader(const char* vertexPath, const char* fragmentPath);
+	Shader(const char* vertexPath, const char* fragmentPath) {
+		// retrieve source code from file paths
+		std::string vertexCodeStr, fragmentCodeStr;
+		std::ifstream vertexFile, fragmentFile;
+		vertexFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+		fragmentFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+
+		try {
+			// open files
+			vertexFile.open(vertexPath);
+			fragmentFile.open(fragmentPath);
+			std::stringstream vertexStream, fragmentStream;
+
+			// read file's buffer content into streams
+			vertexStream << vertexFile.rdbuf();
+			fragmentStream << fragmentFile.rdbuf();
+
+			// close file handlers
+			vertexFile.close();
+			fragmentFile.close();
+
+			// convert stream into string
+			vertexCodeStr = vertexStream.str();
+			fragmentCodeStr = fragmentStream.str();
+		} 
+		catch (std::ifstream::failure e) {
+			std::cout << "ERROR::SHADER::FILE_NOT_SUCCESSFULLY_READ" << std::endl;
+		}
+
+		const char* vertexShaderCode = vertexCodeStr.c_str();
+		const char* fragmentShaderCode = fragmentCodeStr.c_str();
+	}
 
 	// activates shader program
 	void use(); 
